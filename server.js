4db4076a813defa747304e9048672f9417da9d25
Jayendra2688 <jayendrareddy2688@gmail.com>
2024-12-31 20:20:18 +0530

    third commit bmical

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
 app.get('/',function(req,res){
     res.sendFile(__dirname +"/index.html");
 })
@@ -10,10 +11,17 @@ app.get('/',function(req,res){
 app.post("/",function(req,res){
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
-    console.log(num1);
+    // console.log(num1);
     res.send("result is " + (num1+num2));
 })
-
+app.get('/bmical',function(req,res){
+    res.sendFile(__dirname + '/bmi.html');
+})
+app.post('/bmical',function(req,res){
+    var h = Number(req.body.height);
+    var w = Number(req.body.weight);
+    res.send("your bmi is "+Math.round((w/h**2)*100)/100);
+})
 app.listen(3000,function(){
     console.log("sever is active at 3000");
 })
