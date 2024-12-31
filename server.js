4a5f1902f697d3d7c44efe0a9f0a78b33b5fc36d
Jayendra2688 <jayendrareddy2688@gmail.com>
2024-12-31 20:12:38 +0530

    second comm body-parser

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
 app.get('/',function(req,res){
     res.sendFile(__dirname +"/index.html");
+})
+
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    console.log(num1);
+    res.send("result is " + (num1+num2));
 })
 
 app.listen(3000,function(){
